93022216df990e8f11603ee21b9091f2d4fe0ccb
Abhi <abhinav118@gmail.com>
2025-08-19 16:37:52 -0400

    Add components/TestButton.tsx

---
+++ components/TestButton.tsx
@@ -0,0 +1,12 @@
+interface TestButtonProps {
+  variant?: any
+  size?: any
+}
+
+export default function TestButton({ variant, size }: TestButtonProps) {
+  return (
+    <div className="">
+      {/* TestButton component content */}
+    </div>
+  )
+}
