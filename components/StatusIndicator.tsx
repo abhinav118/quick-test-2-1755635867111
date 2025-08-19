b5bbe2b03a15a7555ada5212aeccbd850240ada1
Abhi <abhinav118@gmail.com>
2025-08-19 16:37:53 -0400

    Add components/StatusIndicator.tsx

---
+++ components/StatusIndicator.tsx
@@ -0,0 +1,11 @@
+interface StatusIndicatorProps {
+  status?: any
+}
+
+export default function StatusIndicator({ status }: StatusIndicatorProps) {
+  return (
+    <div className="">
+      {/* StatusIndicator component content */}
+    </div>
+  )
+}
